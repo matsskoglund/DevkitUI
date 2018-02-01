c03719d5ae8327fa266701e428b1ed452a8a65c0
Mats Skoglund <mats.skoglund@scania.com>
2018-02-01 19:49:57 +0100

    Work

--- src/app/modifydevkit.component.ts
+++ src/app/modifydevkit.component.ts
@@ -1,135 +1,71 @@
-import { Component, Input, Inject ,ViewEncapsulation } from '@angular/core';
+import { Component, Input, Inject  } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { FormsModule } from '@angular/forms';  
 import {ActivatedRoute} from "@angular/router";
 import { environment } from '../environments/environment';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
-  selector: 'modifydevkit',
+  selector: 'devkit',
   templateUrl: './modifydevkit.component.html'
+ 
 })
-
-export class ModifyDevkitComponent {  
-  public sDescription = "";
-  public sShortName = "";
-  public sName = "";
-  public sDesigner = "";
-  public ToolsInventory: ToolsMaster[] = [];
-  public newToolSet: ToolsMaster[] = [];
-  public toolsLeft: ToolsMaster[] = [];
-  public selectedTool:Tool; 
-  public newTool:boolean = false;
-  bseUrl:string;
+export class ModifyDevkitComponent {
+  title = 'Devkit';
+  public editMode: boolean = false;
+  public ToolsInventory: InventoryMaster[] = [];
   public Devkit: DevkitMaster[] = [];
-  public selectedToolId: number;
-  private newAttribute: any = {};
+  AddTable: Boolean = false;  
+  public sToolID: number = 0;
+  public sAquire = "";
+  public sAquireType = "";
+  public sDescription = "";
+  public sName = "";
+  public sUrlRef = "";
+  public bseUrl: string = ""; 
+  myName: string;
 
-  constructor(public http: Http)   
+  constructor(public http: Http, @Inject('BASE_URL') baseUrl: string, private route: ActivatedRoute) {
+    this.myName = "Devkit"; 
+    this.bseUrl = environment.apiUrl;
+    this.route.params.subscribe( params => 
+      {
+        this.getData(params['id']);
+        this.getDevKitData(params['id']);
+      }
+    );
+  }
+
+  handleClick(){
+      this.editMode =true;
+  }
+
+  save(devkit: DevkitMaster)
   {
-    this.bseUrl = environment.apiUrl; 
-    this.getToolsData();   
+    this.editMode = false; 
+  }
+  getData(devkitid: number) {
+    this.http.get(this.bseUrl + 'api/Devkits/tools/' + devkitid).subscribe(result => {
+        this.ToolsInventory = result.json();
+        
+    }, error => console.error(error));        
   }
 
-  submitted = false;
-  onSubmit(devkit:any) { 
-    this.submitted = true; 
-    console.log("Submitted");
-    console.log(devkit);
-  }
   getDevKitData(devkitid: number) {
     this.http.get(this.bseUrl + 'api/Devkits/' + devkitid).subscribe(result => {
         this.Devkit = result.json();
     }, error => console.error(error));        
   }
-  /*addTool(){
-    this.newTool = true;
-
-    this.selectedTool = {  toolID: 0,
-      name: "",
-      description: "",
-      aquire: "",
-      urlRef: "",
-      aquireType: "",
-      taken: false,
-     idx: 0};
-  }*/
-
-  deleteTool(Id:number){
-    console.log("deleting " + Id);
-    var obj = this.newToolSet.splice(Id,1);
-    this.toolsLeft.push(obj[0]);
-  }
-
-  addDevkit(sname:string, devkitName: string, devkitDescription: string, designer: string ) {
-    console.log("addDevKit");
-    console.log(sname);
-    var headers = new Headers();
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-  /*  this.http.post(this.bseUrl + 'api/Devkits/', JSON.stringify({ shortName: sname, name: devkitName, description: devkitDescription, email: designer}),
-            { headers: headers }).subscribe(
-            response => {
-                this.getDevKitData(1);
-
-            }, error => {
-            }
-            );*/
-    var theDevkit:DevkitMaster;
-    const req = this.http.post(this.bseUrl + 'api/Devkits/', JSON.stringify({ shortName: sname, name: devkitName, description: devkitDescription, email: designer}),
-            { headers: headers });
-    req.subscribe(
-            response => {
-              theDevkit = response.json();
-                console.log(theDevkit);
-                console.log("Json");
-                console.log(response.json());
-                for (var i = 0, len = this.newToolSet.length; i < len; i++) {                         
-                  var req2 = this.http.post(this.bseUrl + 'api/Devkits/devkitstools/', JSON.stringify({ DevkitID: theDevkit.devkitID, ToolId: this.newToolSet[i].toolID, ToolType: "Core"}),
-                  { headers: headers });
-                  req2.subscribe(
-                  response => {
-                    console.log(response);
-                  }, error => {
-                  }
-                );
-              }
-            }, error => {
-            }
-            );          
-
-           
-    }
-
-    getToolsData() {
-      this.http.get(this.bseUrl + 'api/Tools/').subscribe(result => {
-          this.ToolsInventory = result.json();
-          this.toolsLeft = result.json();
-      
-      }, error => console.error(error));        
-    }
-    onSelect(idx:number) { 
-      console.log(idx);
-      for (var i = 0; i < this.toolsLeft.length; i++)
-      {        
-        if (this.toolsLeft[i].toolID == this.selectedToolId) {
-          console.log(this.toolsLeft[i].toolID);
-          this.selectedTool = this.toolsLeft[i];
-          
-        }
-      }
-    }
-    addNewTool(){
-      this.newToolSet.push(this.selectedTool); 
-      var idx = this.toolsLeft.indexOf(this.selectedTool);
-
-      var obj = this.toolsLeft.splice(idx,1);
-      this.selectedTool = null;
-    }
-
-    
-
 }
 
+export interface InventoryMaster {
+  toolID: number;
+  aquire: string;
+  aquireType: string;
+  categoryID: number;
+  description: string;
+  name: string;
+  urlRef: string;    
+}  
 
 export interface DevkitMaster {
   devkitID: number;
@@ -137,26 +73,4 @@ export interface DevkitMaster {
   name: string;
   description: string;
   email: string;
-}  
-
-export interface ToolsMaster {
-  toolID: number;
-  name: string;
-  description: string;
-  aquire: string;
-  urlRef: string;
-  aquireType: string;
-  taken:boolean;
-  idx: number;
-}  
-
-class Tool implements ToolsMaster{
-  toolID: number;
-  name: string;
-  description: string;
-  aquire: string;
-  urlRef: string;
-  aquireType: string;
-  taken: boolean;
- idx: number;
 }  
