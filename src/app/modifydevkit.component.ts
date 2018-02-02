83213e36a975e1d0aa73ac42fe2bae879a49abc7
Mats Skoglund <mats.skoglund@scania.com>
2018-02-02 17:50:00 +0100

    Edit

--- src/app/modifydevkit.component.ts
+++ src/app/modifydevkit.component.ts
@@ -13,6 +13,8 @@ export class ModifyDevkitComponent {
   title = 'Devkit';
   public editMode: boolean = false;
   public ToolsInventory: InventoryMaster[] = [];
+  public DevkitToolsInventory: InventoryMaster[] = [];
+  public toolsLeft: InventoryMaster[] = [];
   public Devkit: DevkitMaster[] = [];
   AddTable: Boolean = false;  
   public sToolID: number = 0;
@@ -23,33 +25,69 @@ export class ModifyDevkitComponent {
   public sUrlRef = "";
   public bseUrl: string = ""; 
   myName: string;
-
+  public selectedToolId: number;
+  public selectedTool:Tool; 
   constructor(public http: Http, @Inject('BASE_URL') baseUrl: string, private route: ActivatedRoute) {
     this.myName = "Devkit"; 
     this.bseUrl = environment.apiUrl;
     this.route.params.subscribe( params => 
       {
-        this.getData(params['id']);
-        this.getDevKitData(params['id']);
+        this.getDevkitTools(params['id']);
+        this.getDevKitData(params['id']);   
+        this.getAllTools();       
       }
     );
   }
 
-  handleClick(){
+  edit(){
       this.editMode =true;
+   
+      
+      this.updateToolsLeft();
+  }
+
+ onSelect(selectedToolId:number){
+
+   this.selectedTool = this.ToolsInventory[selectedToolId];
+ }
+  updateToolsLeft(){
+    this.toolsLeft = this.ToolsInventory.slice(0);
+
+
+    for( var i=this.toolsLeft.length - 1; i>=0; i--){
+       for( var j=0; j<this.DevkitToolsInventory.length; j++){
+           if(this.toolsLeft[i] && (this.toolsLeft[i].toolID === this.DevkitToolsInventory[j].toolID)){
+            this.toolsLeft.splice(i, 1);
+          }
+        }
+    }
+
   }
 
   save(devkit: DevkitMaster)
   {
     this.editMode = false; 
   }
-  getData(devkitid: number) {
+  /*getData(devkitid: number) {
     this.http.get(this.bseUrl + 'api/Devkits/tools/' + devkitid).subscribe(result => {
         this.ToolsInventory = result.json();
         
     }, error => console.error(error));        
-  }
+  }*/
 
+  getDevkitTools(devkitid: number) {
+    this.http.get(this.bseUrl + 'api/Devkits/tools/' + devkitid).subscribe(result => {
+        this.DevkitToolsInventory = result.json();
+        
+    }, error => console.error(error));        
+  }
+  getAllTools() {
+    this.http.get(this.bseUrl + 'api/Tools/').subscribe(result => {
+        this.ToolsInventory = result.json();
+
+    }, error => console.error(error));        
+  }
+  
   getDevKitData(devkitid: number) {
     this.http.get(this.bseUrl + 'api/Devkits/' + devkitid).subscribe(result => {
         this.Devkit = result.json();
@@ -73,4 +111,13 @@ export interface DevkitMaster {
   name: string;
   description: string;
   email: string;
+}  
+class Tool implements InventoryMaster{
+  toolID: number;
+  aquire: string;
+  aquireType: string;
+  categoryID: number;
+  description: string;
+  name: string;
+  urlRef: string; 
 }  
